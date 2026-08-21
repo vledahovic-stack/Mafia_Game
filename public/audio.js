7790f1a523aa4e7444c8dce6e392c3eaa7ea377e
vledahovic-stack <vledahovic@gmail.com>
2026-08-21 23:44:05 +0300

    Add files via upload

--- public/audio.js
+++ public/audio.js
@@ -38,7 +38,19 @@ export const AudioModule = {
     }
 
     const pc = new RTCPeerConnection({
-      iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+        iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            {
+                urls: 'turn:openrelay.metered.ca:80',
+                username: 'openrelay',
+                credential: 'openrelay'
+            },
+            {
+                urls: 'turn:openrelay.metered.ca:443',
+                username: 'openrelay',
+                credential: 'openrelay'
+            }
+        ]
     });
 
     this.iceCandidateQueues[targetUserId] = [];
