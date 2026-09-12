db35eedd603a5405180e47a4d2c0d6f49fb04b96
vledahovic-stack <vledahovic@gmail.com>
2026-09-12 21:40:56 +0300

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -299,3 +299,15 @@ document.getElementById('btn-logout').addEventListener('click', async () => {
     localStorage.removeItem('username');
     setLoggedOutUser();
 });
+
+// Блокировка масштабирования двухпальцевым жестом на Android
+document.addEventListener('touchmove', function(e) {
+    if (e.touches.length > 1) {
+        e.preventDefault();
+    }
+}, { passive: false });
+
+// Блокировка двойного тапа
+document.addEventListener('dblclick', function(e) {
+    e.preventDefault();
+}, { passive: false });
